95f72cb3b9bb6538a5daf12953b55e6435439841
Anton Kokorev <antonkokorev114@gmail.com>
2024-01-22 11:45:49 +0300

    Refactoring: - Created Modal, Tabs components; - Created types file - Added types for constants;

--- src/SignaturePad/SignaturePad.tsx
+++ src/SignaturePad/SignaturePad.tsx
@@ -1,9 +1,8 @@
 import React, { useState } from 'react';
 import SignatureCanvas from 'react-signature-canvas';
-import ReactSignatureCanvas from 'react-signature-canvas';
 import html2canvas from 'html2canvas';
-import { penColorOptions, typeFontFamilyOptions } from './constants/signaturePad.constants';
-import { FormControlLabel, Radio, RadioGroup, Tab, Tabs } from '@mui/material';
+import SignaturePadModal from './components/SignaturePadModal/SignaturePadModal';
+import { penColorOptions, typeFontFamilyOptions } from './signaturePad.constants';
 import './SignaturePad.css';
 import '@fontsource/caveat/700.css';
 import '@fontsource/pacifico';
@@ -17,7 +16,7 @@ const SignaturePad = () => {
   const [selectedPenColorOption, setSelectedPenColorOption] = useState(penColorOptions[0]);
   const [typedSignatureValue, setTypedSignatureValue] = useState('');
   const [selectedSignatureFontFamily, setSelectedSignatureFontFamily] = useState(typeFontFamilyOptions[0]);
-  let canvasRef: ReactSignatureCanvas | null = null;
+  let canvasRef: SignatureCanvas | null = null;
 
   const handlePenColorChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = event.target;
@@ -26,10 +25,6 @@ const SignaturePad = () => {
     if (newPenValue) {
       setSelectedPenColorOption(newPenValue);
     }
-  }
-
-  const onTabChange = (_: React.SyntheticEvent, newValue: string) => {
-    setSelectedTab(newValue);
   }
 
   const onTypeSignatureChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -43,6 +38,10 @@ const SignaturePad = () => {
     if (newFontValue) {
       setSelectedSignatureFontFamily(newFontValue);
     }
+  }
+
+  const setCanvasRef = (ref: SignatureCanvas | null) => {
+    canvasRef = ref;
   }
 
   const onClearSignatureClick = () => {
@@ -87,7 +86,7 @@ const SignaturePad = () => {
     return canvas.toDataURL();
   };
 
-  const onDoneClick = async () => {
+  const onDoneClick = async (selectedTab: string) => {
     if (selectedTab === 'draw') {
       if (!canvasRef) {
         return;
@@ -111,106 +110,19 @@ const SignaturePad = () => {
         </div>
       </figure>
       {isModalOpen && (
-        <div className="signature-pad-modal">
-          <div className="signature-pad-modal-wrapper">
-            <p className="signature-pad-modal-header">
-              Add Signature
-            </p>
-            <Tabs className="signature-pad-modal-tabs" value={selectedTab} onChange={onTabChange}>
-              <Tab id="tab-draw" label="Draw" value="draw" />
-              <Tab id="tab-type" label="Type" value="type" />
-            </Tabs>
-            <div className="signature-pad-modal-content">
-              <div className="signature-pag-pen-color-selector">
-                {penColorOptions.map((option) => (
-                  <label
-                    key={`color_${option.id}`}
-                    className={`signature-pag-pen-color-label ${
-                      option.id === selectedPenColorOption.id &&
-                      'signature-pag-pen-color-option-selected'
-                    }`}
-                    title={option.label}
-                  >
-                    <span
-                      className="signature-pag-pen-color-span"
-                      style={{ backgroundColor: option.color }}
-                    />
-                    <input
-                      type="radio"
-                      className="signature-pag-pen-color-input"
-                      value={option.id}
-                      checked={option.id === selectedPenColorOption.id}
-                      onChange={handlePenColorChange}
-                    />
-                  </label>
-                ))}
-              </div>
-              {selectedTab === 'draw' && (
-                <>
-                  <div className="signature-pad-modal-canvas">
-                    <SignatureCanvas
-                      ref={(ref) => canvasRef = ref}
-                      penColor={selectedPenColorOption.color}
-                      canvasProps={{ className: 'signature-canvas' }}
-                    />
-                  </div>
-                  <button className="clear-signature-button" onClick={onClearSignatureClick}>
-                    Clear Signature
-                  </button>
-                </>
-              )}
-              {selectedTab === 'type' && (
-                <>
-                  <div className="signature-pad-type-wrapper">
-                    <input
-                      className="signature-pad-type-input"
-                      placeholder="Signature"
-                      style={{
-                        fontFamily: `"${selectedSignatureFontFamily.value}", cursive`,
-                        color: selectedPenColorOption.color
-                      }}
-                      value={typedSignatureValue}
-                      onChange={onTypeSignatureChange}
-                    />
-                    <button className="clear-signature-button" onClick={onClearTypedSignatureClick}>
-                      Clear Signature
-                    </button>
-                  </div>
-                  <div className="signature-pad-font-options">
-                    <RadioGroup
-                      name="font-type-selector"
-                      className="signature-pad-font-radio-group"
-                      onChange={onFontFamilyChange}
-                      row
-                    >
-                      {typeFontFamilyOptions.map((option) => (
-                        <FormControlLabel
-                          className="font-type-option"
-                          label="Signature"
-                          key={`font_${option.id}`}
-                          value={option.id}
-                          checked={option.id === selectedSignatureFontFamily.id}
-                          control={<Radio />}
-                          sx={{
-                            '.MuiFormControlLabel-label': {
-                              fontFamily: `"${option.value}", cursive`,
-                              fontSize: '30px',
-                              color: selectedPenColorOption.color
-                            }
-                          }}
-                        />
-                      ))}
-                    </RadioGroup>
-                  </div>
-                </>
-              )}
-            </div>
-            <div className="signature-pad-modal-footer">
-              <button onClick={closeModal}>Cancel</button>
-              <button className="done-button" onClick={onDoneClick}>Done</button>
-            </div>
-          </div>
-        </div>
+        <SignaturePadModal
+          selectedPenColorOption={selectedPenColorOption}
+          selectedSignatureFontFamily={selectedSignatureFontFamily}
+          typedSignatureValue={typedSignatureValue}
+          setCanvasRef={setCanvasRef}
+          closeModal={closeModal}
+          handlePenColorChange={handlePenColorChange}
+          onClearSignatureClick={onClearSignatureClick}
+          onClearTypedSignatureClick={onClearTypedSignatureClick}
+          onTypeSignatureChange={onTypeSignatureChange}
+          onFontFamilyChange={onFontFamilyChange}
+          onDoneClick={onDoneClick}
+        />
       )}
     </div>
   );
